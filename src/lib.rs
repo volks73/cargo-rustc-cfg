04bdab65706305a59edc18d77564edb599df9b57
Chris Field <chris@fieldrndservices.com>
2021-03-01 20:24:04 -0500

    Add TODO

--- src/lib.rs
+++ src/lib.rs
@@ -705,6 +705,7 @@ impl CargoRustcPrintCfg {
             cmd.arg(arg);
         }
         cmd.arg(RUSTC);
+        // TODO: Remove the `-Z unstable-options` once the `--print` argument is stable.
         cmd.arg("-Z");
         cmd.arg("unstable-options");
         if self.rustc_targets.len() > 1 {
